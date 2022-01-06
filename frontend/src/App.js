6adb9067632c95ebcec9f58829197bea1f94bfba
Your Name <newsonthokchom@gmail.com>
2022-01-07 01:58:45 +0530

    React is set up and created Homepage and prdoct page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,39 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
-      <h1>
-        Welcome to LoneCoderShop
-      </h1>
-    </>
+    <Router>
+
+
+      <Header />
+
+      <main className="py-3">
+        <Container>
+          {/* <Routes>
+            <Route path='/' component={HomeScreen} exact />
+          </Routes> */}
+
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} exact />
+          </Routes>
+          {/* <HomeScreen /> */}
+        </Container>
+      </main>
+
+      <Footer />
+
+
+
+
+    </Router>
+
   );
 }
 
