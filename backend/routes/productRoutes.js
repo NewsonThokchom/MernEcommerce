0513b7d807b94842be39f40473d294a971e8b075
Your Name <newsonthokchom@gmail.com>
2022-02-16 19:51:31 +0530

    middleware folder and errorMiddleware.js is created and notFound,errorHandler function is added to errorMiddleware.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -20,12 +20,15 @@ router.get('/', asyncHandler(async (req, res) => {
 router.get('/:id', asyncHandler(async (req, res) => {
     // const product = products.find(p => p._id === req.params.id)
     const product = await Product.findById(req.params.id)
+
     if (product) {
         res.json(product)
     } else {
         // res.status(404).json({ message: 'Product not found!' })
-        res.status(404).json({ error: 'Product not found' });
-        return;
+        // res.status(404).json({ error: 'Product not found' }); 
+        res.status(404)
+        throw new Error('Product not found')
+
     }
 })
 )
