d3551b16fc1f51108f39b3aea9edb3782d666e4b
Your Name <newsonthokchom@gmail.com>
2022-02-19 09:06:20 +0530

    actions/productActions.js,constant/productConstant.js, reducers/productReducers.js is created and add productListReducer inside combineReducers() in store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,18 +2,26 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+import { productListReducer } from './reducers/productReducers';
+
 
 //combineReducers turns an object whose values are different reducer functions,into a single reducer function.
 //It will call every child reducer,and gather their results into a single state object,
 // whose keys correspond to the key of the passed reducer function 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    productList: productListReducer,
+})
 
 const initialState = {}
 
 const middleware = [thunk]
 
 //createStore holds the state tree
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
 
 
 export default store
