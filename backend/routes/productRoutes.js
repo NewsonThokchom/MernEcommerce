70ce023ca8e18fcffffb4213c02670b4cddb3ac5
Your Name <newsonthokchom@gmail.com>
2022-03-30 23:14:48 +0530

    back end product route cleanup by using productController

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,36 +1,10 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
+import { getProductById, getProducts } from '../controller/productController.js'
+
+
 const router = express.Router()
-import Product from '../models/productModel.js'
 
-
-//@desc Fetch all products
-//@route GET/api/products
-//@access Public
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-})
-)
-
-
-//@desc Fetch single products
-//@route GET/api/products/:id
-//@access Public
-router.get('/:id', asyncHandler(async (req, res) => {
-    // const product = products.find(p => p._id === req.params.id)
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-        res.json(product)
-    } else {
-        // res.status(404).json({ message: 'Product not found!' })
-        // res.status(404).json({ error: 'Product not found' }); 
-        res.status(404)
-        throw new Error('Product not found')
-    }
-})
-)
-
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
