83fae98987a7698a50b19fd06b30a38099f48cb7
Your Name <newsonthokchom@gmail.com>
2022-03-31 00:54:08 +0530

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
 
 dotenv.config()
 
@@ -12,11 +14,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API IS RUNNING....')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 //use middleWare function from errorMiddleware.js file
 app.use(notFound)
