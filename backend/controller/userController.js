8c0f97cd352e4e4396cdf6763d2a5348ad9d53ba
Your Name <newsonthokchom@gmail.com>
2022-04-02 23:26:46 +0530

    Custom Authentication Middleware

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc Auth user & get token
 // @route POST /api/users/login
@@ -15,16 +16,37 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
         throw new Error('Invalid email or password.')
     }
-
-    // res.send({
-    //     email,
-    //     password,
-    // })
-
+    // res.send({email,password, })
 })
 
-export { authUser }
+
+// @desc Get user profile
+// @route POST /api/users/profile
+// @access private
+const getUserProfile = asyncHandler(async (req, res) => {
+
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+
+    } else {
+
+        res.status(404)
+        throw new Error('User not found.')
+
+    }
+})
+
+
+export { authUser, getUserProfile }
