e8920d2b9c67c0bde3a39df924351c92047a6b86
Your Name <newsonthokchom@gmail.com>
2022-04-04 01:10:50 +0530

    User login reducer & action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 
 //combineReducers turns an object whose values are different reducer functions,into a single reducer function.
@@ -12,18 +13,22 @@ import { cartReducer } from './reducers/cartReducers';
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems'))
     : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null
+
 
 const initialState = {
-    cart: {
-        cartItems: cartItemsFromStorage
-    }
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
